1bb9a51d5246bfd0604e5083fdeb33b6f81dd5b4
Kartikey Mishra <kartikmishra686@gmail.com>
2022-12-14 21:48:59 +0530

    Basic Template For webSite Added

---
+++ Cronometer_App/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Allroute from "./pages/Allroute.jsx"
+
+
+function App() {
+  return (
+    <div >
+      <Allroute/>
+    </div>
+  );
+}
+
+export default App;
